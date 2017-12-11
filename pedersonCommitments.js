ed8b4d157e020d643b6a78f8cfd6e18bc6693895
Coenie Beyers <coeniebeyers@gmail.com>
2017-12-11 18:15:54 +0200

    Added basic pederson commitments + blinding factors

--- pedersonCommitments.js
+++ pedersonCommitments.js
@@ -1,10 +1,11 @@
-var EC = require('elliptic').ec;
-var ec = new EC('secp256k1');
+let EC = require('elliptic').ec
+let ec = new EC('secp256k1')
 let bn = require('bn.js')
 
 let startTime = new Date().getTime()
 
-var P = ec.genKeyPair();
+let P = ec.genKeyPair()
+let Q = ec.genKeyPair()
 
 if(process.argv.length !== 5){
   console.log('Please pass in two input values (i1, i2) and one output value (v)')
@@ -16,15 +17,26 @@ let arg2 = process.argv[2]
 let arg3 = process.argv[3]
 let arg4 = process.argv[4]
 
+// These blinding factors (r1,r2) are not how this should work in production!!
+let r1 = new bn(Math.floor(Math.random()*Number.MAX_SAFE_INTEGER).toString(), 10)
+let r2 = new bn(Math.floor(Math.random()*Number.MAX_SAFE_INTEGER).toString(), 10)
+let r3 = r1.add(r2)
 let i1 = new bn(arg2, 10)
 let i2 = new bn(arg3, 10)
 let v = new bn(arg4, 10)
 
-let c1 = P.getPublic().mul(i1)
-let c2 = P.getPublic().mul(i2)
-let c1c2 = c1.add(c2)
-let c3 = P.getPublic().mul(v)
+let i1P = P.getPublic().mul(i1)
+let i2P = P.getPublic().mul(i2)
+let r1Q = Q.getPublic().mul(r1)
+let r2Q = Q.getPublic().mul(r2)
+let vP = P.getPublic().mul(v)
+let r3Q = Q.getPublic().mul(r3)
 
+let c1 = (i1P).add(r1Q)
+let c2 = (i2P).add(r2Q)
+let c3 = (vP).add(r3Q)
+
+let c1c2 = c1.add(c2)
 let equalX = c1c2.getX().cmp(c3.getX())
 let equalY = c1c2.getX().cmp(c3.getX())
 
