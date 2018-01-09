4e7d7dad0b91eecedab2c838b586ba2941db90a1
Ryno Beyers <rynobey@gmail.com>
2018-01-09 23:49:34 +0200

    Fixed issues, should work now

--- rangeProofs.js
+++ rangeProofs.js
@@ -1,10 +1,11 @@
+let fs = require('fs');
 let bn = require('bn.js');
 let DRBG = require('hmac-drbg');
 let hash = require('hash.js');
 let randomInt = require('random-int');
 let EC = require('elliptic').ec;
 let ec = new EC('secp256k1');
-let ring = require('./ECCRingSignatures/ring.js')('a59bb438a857d831f85ead86ff3e2d68b2ea8d9ea8b5fdd4ca7739a9f50be37d');
+let ring = require('./ECCRingSignatures/ring.js')();
 
 // Initialization of DRBG
 let r = new DRBG({
@@ -20,10 +21,12 @@ let toxicWaste = new bn(r.generate(32, 'hex'), 16);
 let H = G.mul(toxicWaste);
 
 // Number of bits
-let bits = 2
+let bits = 64;
+
+let startTime = new Date().getTime();
 
 // This is the value that we want to hide. We need to prove that it is positive.
-let v = 2;
+let v = -234523456;
 
 // This is the secret blinding factor used to hide the value that is committed to
 let b = new bn(r.generate(32, 'hex'), 16);
@@ -47,15 +50,15 @@ for (let i = 0; i < bits; i++) {
 }
 
 // sanity check: Sum over the 64 individual blinding factors, which should equal initial b
-let check1 = new bn(0, 16);
-for (let i = 0; i < bits; i++) {
-  check1 = check1.add(bi[i]);
-}
-console.log();
-console.log("Blinding factor:", b);
-console.log("Sanity check1: Should equal blinding factor:", check1);
-console.log("Equal: ", b.eq(check1));
-console.log();
+//let check1 = new bn(0, 16);
+//for (let i = 0; i < bits; i++) {
+//  check1 = check1.add(bi[i]);
+//}
+//console.log();
+//console.log("Blinding factor:", b);
+//console.log("Sanity check1: Should equal blinding factor:", check1);
+//console.log("Equal: ", b.eq(check1));
+//console.log();
 
 // Get the (unsigned) binary representation v_bin of v 
 let v_bin = BNToNBitString(new bn(v, 10), bits);
@@ -67,19 +70,19 @@ for (let i = 0; i < bits; i++) {
 }
 
 // sanity check: Sum over the 64 individual commitments, which should equal initial C
-let check2;
-for (let i = 0; i < bits; i++) {
-  if (i == 0) {
-    check2 = Ci[i].getPublic();
-  } else {
-    check2 = check2.add(Ci[i].getPublic());
-  }
-}
-console.log();
-console.log("Original commitment C:", C);
-console.log("Sanity check2: Should equal C:", check2);
-console.log("Equal: ", C.eq(check2));
-console.log();
+//let check2;
+//for (let i = 0; i < bits; i++) {
+//  if (i == 0) {
+//    check2 = Ci[i].getPublic();
+//  } else {
+//    check2 = check2.add(Ci[i].getPublic());
+//  }
+//}
+//console.log();
+//console.log("Original commitment C:", C);
+//console.log("Sanity check2: Should equal C:", check2);
+//console.log("Equal: ", C.eq(check2));
+//console.log();
 
 
 /* Generate a ring signature for each individual commitment, using pubsA = [C0, C1, C2, ..., C63] for one of the two public keys, and pubsB = [C0 - 1H, C1 - 2H, C2 - 4H, C3 - 8H, ..., C63 - 9223372036854775808H] for the other.
@@ -98,69 +101,91 @@ for (let i = 0; i < bits; i++) {
   let B = ec.keyFromPublic(H.mul(getBitEquivalentValue(v_bin_max, i, bits)));
   pubsB[i] = subtractBFromA(pubsA[i], B);
 }
-console.log(pubsA)
-console.log(pubsB)
   
 // Get inverse of (unsigned) binary representation Nv_bin of v. We know the private keys of the elements in pubsA where Nv_bin=1, and in pubsB where v_bin=1.
 let Nv_bin = BNToNBitString((new bn(v, 10)).notn(bits), bits);
 
 // Generate ring signatures
 let msg = "veritaserum";
+let sigs = [];
+let keyPairs = [];
+for (let i = 0; i < bits; i++) {
+  if (getBitAtPosition(v_bin, i, bits).eq(new bn(1, 10))) {
+    keyPairs[i] = [
+      pubsA[i],
+      ec.keyFromPrivate(bi[i])
+    ];
+    sigs[i] = ring.Sign(keyPairs[i], msg, 1);
+  //} else if (getBitAtPosition(Nv_bin, i, bits).eq(new bn(1, 10))) {
+  } else {
+    keyPairs[i] = [
+      ec.keyFromPrivate(bi[i]),
+      pubsB[i]
+    ];
+    sigs[i] = ring.Sign(keyPairs[i], msg, 0);
+  }
+}
 
-// wtf...
-let keyPairs = [
-  ec.keyFromPrivate(bi[0]),
-  pubsA[0]
-];
+let endTimeProof = new Date().getTime();
 
-let pubKey = keyPairs[1].getPublic();
-let sharedPrivKey = new bn('a59bb438a857d831f85ead86ff3e2d68b2ea8d9ea8b5fdd4ca7739a9f50be37d', 16);
-//let sharedPrivKey = new bn(r.generate(32, 'hex'), 16);
-let sharedKeyPair = ec.keyFromPrivate(sharedPrivKey);
-let newSharedPrivKey = sharedKeyPair.getPrivate();
-let val = ec.keyFromPublic(pubKey.mul(sharedPrivKey));
-let valError = val.getPublic().add(val.getPublic());
+// Verify the ring signatures
+let ringSigValid = true;
+for (let i = 0; i < bits; i++) {
+  ringSigValid = ringSigValid && ring.Verify(keyPairs[i], msg, sigs[i]);
+}
 
-//let x = ec.keyFromPrivate(new bn(r.generate(32, 'hex'), 16));
-let x = ec.keyFromPrivate(new bn('0e68c8f68ec6a780bacc80c136135353693e2b97ba5aa20c3f85ecba3e948270', 16));
-let xx = ec.keyFromPublic(x.getPublic());
-console.log("CALLING f WITH PARAMS:");
-console.log("PARAM1 = ", xx);
-console.log("PARAM2 = ", keyPairs[1].getPublic());
-console.log("PARAM3 = ", sharedKeyPair.getPrivate());
-ring.F(xx, keyPairs[1].getPublic(), sharedKeyPair.getPrivate());
+// Check that the individual commitments sum to the total commitment.
+// If this is false, the sender probably tried to commit to a negative v,
+// and implies that the ring signature (if valid) is for a different v than they
+// intended.
+let check2;
+for (let i = 0; i < bits; i++) {
+  if (i == 0) {
+    check2 = Ci[i].getPublic();
+  } else {
+    check2 = check2.add(Ci[i].getPublic());
+  }
+}
+let commitmentValid = C.eq(check2);
 
-sig = ring.Sign(keyPairs, msg, 0);
+// Final proof outcome
+let totResult = ringSigValid && commitmentValid;
+console.log("Committed to a positive value:", totResult);
 
-//let sigs = [];
-//for (let i = 0; i < bits; i++) {
-//  if (getBitAtPosition(v_bin, i, bits).eq(new bn(1, 10))) {
-//    let keyPairs = [
-//      ec.keyFromPrivate(bi[i]),
-//      pubsB[i]
-//    ];
-//    console.log(keyPairs[0].getPublic())
-//    sigs[i] = ring.Sign(keyPairs, msg, 0);
-//  } else if (getBitAtPosition(Nv_bin, i, bits).eq(new bn(1, 10))) {
-//    let keyPairs = [
-//      ec.keyFromPrivate(bi[i]),
-//      pubsA[i]
-//    ];
-//    console.log(keyPairs[0].getPublic())
-//    sigs[i] = ring.Sign(keyPairs, msg, 0);
-//  }
-//}
-//
-//// Verify the rign signatures
-//let results = [];
-//for (let i = 0; i < bits; i++) {
-//  let pubKeys = [
-//    pubsA[i],
-//    pubsB[i]
-//  ];
-//  results[i] = ring.Verify(pubKeys, msg, sigs[i]);
-//}
-//console.log(results);
+let endTimeVer = new Date().getTime();
+console.log("Proof generation time:", endTimeProof - startTime, "ms");
+console.log("Proof verification time:", endTimeVer - endTimeProof, "ms");
+
+// Write proof to file
+let pubKeys = keyPairs.map(function(pairs) {
+    let pubs = [
+      pairs[0].getPublic(),
+      pairs[1].getPublic()
+    ];
+    return pubs;
+  });
+let sigKeys = sigs.map(function(sig) {
+    let keys = [
+      sig[0].getPublic(),
+      sig[1].getPublic()
+    ];
+    return keys;
+  });
+
+let proof = {
+  publicKeys: pubKeys,
+  msg: msg,
+  sigs: sigKeys
+}
+fs.writeFileSync('./proof.json', JSON.stringify(proof, null, 2), 'utf-8');
+
+
+
+/* *******************************************************************************************
+
+HELPER FUNCTIONS
+
+**********************************************************************************************/
 
 // Takes as input a keyPair (privKey may be null)
 function negate(pos) {
