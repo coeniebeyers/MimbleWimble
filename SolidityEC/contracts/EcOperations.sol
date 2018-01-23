f90948abd14e032e6a8a7eae49527c89592a9bad
Coenie Beyers <coeniebeyers@gmail.com>
2018-01-23 15:36:15 +0200

    Added ecadd

--- SolidityEC/contracts/EcOperations.sol
+++ SolidityEC/contracts/EcOperations.sol
@@ -1,8 +1,19 @@
 pragma solidity ^0.4.19;
 
 contract EcOperations{
- function ecmul(uint256 x, uint256 y, uint256 scalar) public payable returns(uint256[2] p) {
-    // With a public key (x, y), this computes p = scalar * (x, y).
+  function ecadd(uint256 ax, uint256 ay, uint256 bx, uint256 by) public payable returns(uint256[2] p) {
+    uint256[4] memory input;
+    input[0] = ax;
+    input[1] = ay;
+    input[2] = bx;
+    input[3] = by;
+    assembly {
+      if iszero(call(not(0), 0x06, 0, input, 0x80, p, 0x40)) {
+        revert(0, 0)
+      }
+    }
+  } 
+  function ecmul(uint256 x, uint256 y, uint256 scalar) public payable returns(uint256[2] p) {
     uint256[3] memory input;
     input[0] = x;
     input[1] = y;
--- SolidityEC/test/TestEcOperations.sol
+++ SolidityEC/test/TestEcOperations.sol
@@ -4,6 +4,19 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/EcOperations.sol";
 
 contract TestEcOperations{
+  function testEcAdd() public {
+    EcOperations ec = new EcOperations();
+    uint256 ax = 1;
+    uint256 ay = 2;
+    uint256 bx = 1;
+    uint256 by = 2;
+    uint256[2] memory actual = ec.ecadd(ax, ay, bx, by);
+    uint256[2] memory expected;
+    expected[0] = 1368015179489954701390400359078579693043519447331113978918064868415326638035; //x
+    expected[1] = 9918110051302171585080402603319702774565515993150576347155970296011118125764; //y
+    Assert.equal(expected[0], actual[0], "The answer should be a.x + b.x");
+    Assert.equal(expected[1], actual[1], "The answer should be a.y + b.y");
+  }
   function testEcMul() public {
     EcOperations ec = new EcOperations();
     uint256 original_x = 1;
