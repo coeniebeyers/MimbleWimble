412b5abc3e4f0a68ba5ba50f8a45a179256f6c3b
Coenie Beyers <coeniebeyers@gmail.com>
2018-01-23 17:13:22 +0200

    Added test for estimating range proof gas usage. Test is commented due to high gas costs

--- SolidityEC/contracts/EcOperations.sol
+++ SolidityEC/contracts/EcOperations.sol
@@ -25,4 +25,49 @@ contract EcOperations{
       }
     }
   } 
+
+  // upper bound gas cost ~= (2 x ecmul + 1 x ecsub + 1 x sha3 ) x digits x base
+  // for 64 bits: 256 x ecmul + 128 ecsub + 128 sha3
+  function verifyRingSigGasCosts() public payable returns(uint256[2] p) {
+    uint256[4] memory input;
+    input[0] = 1;
+    input[1] = 2;
+    input[2] = 2;
+    assembly {
+      let n := 256
+      let i := 0
+      loop:
+        jumpi(end, eq(i, n))
+        if iszero(call(not(0), 0x07, 0, input, 0x60, p, 0x40)){
+          revert(0, 0) 
+        }
+        i := add(i, 1)
+        jump(loop)
+      end:
+    }
+    input[2] = 1;
+    input[3] = 2;
+    assembly {
+      let n := 128
+      let i := 0
+      loop:
+        jumpi(end, eq(i, n))
+        if iszero(call(not(0), 0x06, 0, input, 0x80, p, 0x40)){
+          revert(0, 0) 
+        }
+        i := add(i, 1)
+        jump(loop)
+      end:
+    }
+    assembly {
+      let n := 128
+      let i := 0
+      loop:
+        jumpi(end, eq(i, n))
+        if iszero(keccak256(1368015179489954701390400359078579693043519447331113978918064868415326638035, 1)){}
+        i := add(i, 1)
+        jump(loop)
+      end:
+    }
+  } 
 }
--- SolidityEC/test/TestEcOperations.sol
+++ SolidityEC/test/TestEcOperations.sol
@@ -29,4 +29,14 @@ contract TestEcOperations{
     Assert.equal(expected[0], actual[0], "The point p.x should be multiplied by the scalar");
     Assert.equal(expected[1], actual[1], "The point p.y should be multiplied by the scalar");
   }
+  // This works, but consumes 10899348 gas and breaks testrpc's defaults
+  /*function testVerifyRingSigGasCosts() public {
+    EcOperations ec = new EcOperations();
+    uint256[2] memory actual = ec.verifyRingSigGasCosts();
+    uint256[2] memory expected;
+    expected[0] = 1368015179489954701390400359078579693043519447331113978918064868415326638035; //x
+    expected[1] = 9918110051302171585080402603319702774565515993150576347155970296011118125764; //y
+    Assert.equal(expected[0], actual[0], "The point p.x should be multiplied by the scalar");
+    Assert.equal(expected[1], actual[1], "The point p.y should be multiplied by the scalar");
+  }*/
 }
